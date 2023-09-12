37476fca4acf34d09f4f3b90d8f9d39baea947d8
Gaurav <4gaurav07@gmail.com>
2023-09-12 19:46:59 +0530

    Footer File Added

---
+++ Hamburger Menu/hamburger.js
@@ -0,0 +1,15 @@
+
+document / addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector(".menu");
+    let menuOpen = false;
+
+    menu.addEventListener('click', function () {
+        if (!menuOpen) {
+            menu.classList.add("open");
+            menuOpen = true;
+        } else {
+            menu.classList.remove("open");
+            menuOpen = false;
+        }
+    });
+});
